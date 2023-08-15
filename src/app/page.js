0702152858641cda07184cd4d8f984a84642aea4
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-08-15 12:37:44 +0530

    ADDED COMPONENT: added navbar mask animation link

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Navbar from "@/components/Navbar/index";
+import Link from "next/link";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 export default function Home() {
-  return (
-    <div className="underline uppercase text-[3vw] font-black text-red-500">
-      Hello World!!!
-    </div>
-  )
+	return (
+		<>
+			<Navbar />
+			<div className="h-[10vh] text-[30px] text-[#ec4e39] flex justify-center items-center">
+				<Link
+					href="Mask-Animation/"
+					className="flex justify-center items-center"
+				>
+					Mask Animation <FaExternalLinkAlt className="ml-2" />
+				</Link>
+			</div>
+		</>
+	);
 }
