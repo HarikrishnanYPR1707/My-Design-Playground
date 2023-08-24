692521c38f2681084cad2f7f4b670d3ef81c3dd3
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-08-24 14:35:06 +0530

    COMPONENT: added home component

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaHome } from "react-icons/fa";
+
+const index = () => {
+	return (
+		<div className="text-white font-black absolute">
+			<FaHome />
+		</div>
+	);
+};
+
+export default index;
