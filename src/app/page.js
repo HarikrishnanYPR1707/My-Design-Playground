902a2caf92ac537b0a87c48edb767387813bf499
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-08-24 14:46:49 +0530

    MODIFIED: page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,26 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar/index";
-import HomeButton from "@/components/Home/index";
 import Link from "next/link";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
 export default function Home() {
-	return (
-		<>
-			<Navbar />
-			<HomeButton />
-			<div className="h-[10vh] text-[30px] text-[#ec4e39] flex flex-col justify-center items-center">
-				<Link
-					href="Mask-Animation/"
-					className="flex justify-center items-center"
-				>
-					Mask Animation <FaExternalLinkAlt className="ml-2" />
-				</Link>
-				<Link
-					href="Blog-Hover-Animation/"
-					className="flex justify-center items-center"
-				>
-					BLOG HOVER ANIMATION <FaExternalLinkAlt className="ml-2" />
-				</Link>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <div className="flex h-[10vh] flex-col items-center justify-center text-[30px] text-[#ec4e39]">
+        <Link
+          href="Mask-Animation/"
+          className="flex items-center justify-center"
+        >
+          Mask Animation <FaExternalLinkAlt className="ml-2" />
+        </Link>
+        <Link
+          href="Blob-Hover-Animation/"
+          className="flex items-center justify-center"
+        >
+          BLOG HOVER ANIMATION <FaExternalLinkAlt className="ml-2" />
+        </Link>
+      </div>
+    </>
+  );
 }
