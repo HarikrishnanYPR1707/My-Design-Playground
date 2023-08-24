9bb854d4bc4cc6e79c2c80141c49eadf84588728
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-08-24 14:46:12 +0530

    HOME BUTTOM: complted

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { FaHome } from "react-icons/fa";
+import Link from "next/link";
 
 const index = () => {
-	return (
-		<div className="text-white font-black absolute">
-			<FaHome />
-		</div>
-	);
+  return (
+    <Link
+      href="/"
+      className="absolute left-10 top-10 z-20 grid aspect-square w-[60px] place-content-center rounded-full bg-[#ec4e39] text-4xl font-black text-[#252525]"
+    >
+      <FaHome />
+    </Link>
+  );
 };
 
 export default index;
