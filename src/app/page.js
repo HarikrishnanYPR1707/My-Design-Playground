612f3f7ef64aa8f428581fa531e853118a4de116
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-08-24 14:33:55 +0530

    LINK FOR BLOG-HOVER-ANIMATION: Added to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar/index";
+import HomeButton from "@/components/Home/index";
 import Link from "next/link";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
@@ -7,12 +8,19 @@ export default function Home() {
 	return (
 		<>
 			<Navbar />
-			<div className="h-[10vh] text-[30px] text-[#ec4e39] flex justify-center items-center">
+			<HomeButton />
+			<div className="h-[10vh] text-[30px] text-[#ec4e39] flex flex-col justify-center items-center">
 				<Link
 					href="Mask-Animation/"
 					className="flex justify-center items-center"
 				>
 					Mask Animation <FaExternalLinkAlt className="ml-2" />
+				</Link>
+				<Link
+					href="Blog-Hover-Animation/"
+					className="flex justify-center items-center"
+				>
+					BLOG HOVER ANIMATION <FaExternalLinkAlt className="ml-2" />
 				</Link>
 			</div>
 		</>
