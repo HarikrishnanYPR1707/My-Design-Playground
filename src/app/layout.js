bcfc8b9acc5d52966132730cd3f5f960157d49ea
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-08-24 14:47:20 +0530

    ADDD COMPONENET: Home Button added for navigation for all pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,22 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
+import "./globals.css";
+import { Inter } from "next/font/google";
+import HomeButton from "@/components/Home/index";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'My Design Playground',
-  description: 'Page where you guys can see all the design created and replicated from other websites',
-}
+  title: "My Design Playground",
+  description:
+    "Page where you guys can see all the design created and replicated from other websites",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <HomeButton />
         {children}
       </body>
     </html>
-  )
+  );
 }
