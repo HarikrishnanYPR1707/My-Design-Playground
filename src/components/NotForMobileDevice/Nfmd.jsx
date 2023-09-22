54da4a934e5bdbc98ce8eed07a0fe2d293e874a7
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-09-22 23:29:51 +0530

    COMPONENT: for not for mobile device page

---
+++ src/components/NotForMobileDevice/Nfmd.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nfmd = () => {
+    return (
+        <div className="h-screen w-screen grid place-content-center font-black text-5xl text-[#2f2f2f]">
+            <p className="">
+                Not for mobile device
+            </p>
+        </div>
+    )
+}
+
+export default Nfmd
