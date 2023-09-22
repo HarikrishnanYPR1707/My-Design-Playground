736fa08af7a648a8c893b63d66a5001e0bbf37c9
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-09-22 23:29:13 +0530

    feature for component visibility above 767px window width

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,15 +1,28 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
+"use client";
 import React from "react";
 import { FaHome } from "react-icons/fa";
 import Link from "next/link";
+import useWindowSize from "@/app/utils/useWindowSize";
 
 const index = () => {
+
+  const { innerWidth, innerHeight } = useWindowSize();
+
   return (
-    <Link
-      href="/"
-      className="absolute left-10 top-10 z-20 grid aspect-square w-[60px] place-content-center rounded-full bg-[#ec4e39] text-4xl font-black text-[#252525]"
-    >
-      <FaHome />
-    </Link>
+    <>
+      {(innerWidth >= 767) ? (
+        <>
+          <Link
+            href="/"
+            className="absolute left-10 top-10 z-20 grid aspect-square w-[60px] place-content-center rounded-full bg-[#ec4e39] text-4xl font-black text-[#252525]"
+          >
+            <FaHome />
+          </Link>
+        </>
+      ) : <></>}
+    </>
   );
 };
 
