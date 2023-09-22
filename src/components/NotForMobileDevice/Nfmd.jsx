da1da7bd4407d6e0013be223f26816fc8f8f467a
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-09-22 23:43:35 +0530

    Changed the notifying comment

--- src/components/NotForMobileDevice/Nfmd.jsx
+++ src/components/NotForMobileDevice/Nfmd.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Nfmd = () => {
     return (
-        <div className="h-screen w-screen grid place-content-center font-black text-5xl text-[#2f2f2f]">
-            <p className="">
-                Not for mobile device
+        <div className="h-screen w-screen px-10 grid place-content-center font-black text-2xl text-[#2f2f2f]">
+            <p className="text-center">
+                Unfortunately, this content is not currently available for mobile devices. I apologize for any inconvenience this may cause and appreciate your understanding. I recommend using your laptop.
             </p>
         </div>
     )
