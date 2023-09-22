82cf4333899310c7eee4fa2ccac325be8f6ee7d6
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-09-22 20:52:40 +0530

    MODIFIED: Created a grid layout

--- src/app/page.js
+++ src/app/page.js
@@ -7,34 +7,34 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="mt-10 flex h-[10vh] flex-col items-center justify-center text-[30px] text-[#ec4e39]">
+      <div className="mt-10 grid grid-cols-3 gap-4 px-10 text-[30px] text-[#ec4e39]">
         <Link
           href="Mask-Cursor-Animation/"
-          className="flex items-center justify-center"
+          className="flex items-center justify-center rounded-xl px-7 py-3 duration-700 hover:bg-[#2e2e2e]"
         >
           Mask Cursor Animation <FaExternalLinkAlt className="ml-2" />
         </Link>
         <Link
           href="Background-Hover-Animation/"
-          className="flex items-center justify-center"
+          className="flex items-center justify-center rounded-xl px-7 py-3 duration-700 hover:bg-[#2e2e2e]"
         >
           Background Hover Animation <FaExternalLinkAlt className="ml-2" />
         </Link>
         <Link
           href="Hacker-Text-Animation/"
-          className="flex items-center justify-center"
+          className="flex items-center justify-center rounded-xl px-7 py-3 duration-700 hover:bg-[#2e2e2e]"
         >
           Hacker Text Animation <FaExternalLinkAlt className="ml-2" />
         </Link>
         <Link
           href="ImageCarousel/"
-          className="flex items-center justify-center"
+          className="flex items-center justify-center rounded-xl px-7 py-3 duration-700 hover:bg-[#2e2e2e]"
         >
           Image Carousel <FaExternalLinkAlt className="ml-2" />
         </Link>
         <Link
           href="ImageRevealHoverAnimation/"
-          className="flex items-center justify-center"
+          className="flex items-center justify-center rounded-xl px-7 py-3 duration-700 hover:bg-[#2e2e2e]"
         >
           Image Reveal Hover Animation <FaExternalLinkAlt className="ml-2" />
         </Link>
