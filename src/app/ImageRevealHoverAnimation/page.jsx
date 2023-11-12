5166fda6c0c0cce8924223ac7cf9ee2485ca467f
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-11-12 07:49:24 +0530

    Added easing to Image Hover Effect

--- src/app/ImageRevealHoverAnimation/page.jsx
+++ src/app/ImageRevealHoverAnimation/page.jsx
@@ -1,41 +1,38 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client";
 
-import React, { useState } from 'react';
-import style from './styles.module.css';
+import React, { useState } from "react";
+import style from "./styles.module.css";
 import { motion } from "framer-motion";
 
 const page = () => {
+  const [isHovered, setIsHovered] = useState(false);
 
-    const [isHovered, setIsHovered] = useState(false);
+  const size = isHovered ? 2100 : 0;
 
-    const size = isHovered ? 2100 : 0;
+  return (
+    <div
+      className={style.main}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {/* Text Container */}
+      <div className={style.textContainer}>
+        <p>Hover To Reveal</p>
+      </div>
+      {/* Image Container */}
+      <motion.div
+        className={style.imageContainer}
+        animate={{
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{
+          duration: 1.8,
+          ease: [0.83, 0, 0.17, 1],
+        }}
+      />
+    </div>
+  );
+};
 
-    return (
-        <div
-            className={style.main}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-        >
-            {/* Text Container */}
-            <div className={style.textContainer}>
-                <p>Hover To Reveal</p>
-            </div>
-            {/* Image Container */}
-            <motion.div
-                className={style.imageContainer}
-                animate={{
-                    WebkitMaskSize: `${size}px`
-                }}
-                transition={{
-                    // type: 'tween',
-                    // ease: "circOut",
-                    duration: 0.75,
-                    // delay: 0.1
-                }}
-            />
-        </div>
-    )
-}
-
-export default page
+export default page;
