ffa370d02a20bc73ab8542de66ce75ba0960d772
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-02 19:19:29 +0530

    Completed with the structure

--- src/app/TextAnimation/component/word.jsx
+++ src/app/TextAnimation/component/word.jsx
@@ -5,22 +5,19 @@ import { useRef } from "react";
 import styles from "../page.module.scss";
 
 const word = ({ value }) => {
-  const word = value.split(" ");
-
   const element = useRef(null);
   const { scrollYProgress } = useScroll({
     target: element,
     offset: ["start 0.9", "start 0.3"],
   });
 
+  const words = value.split(" ");
   return (
-    <motion.p
-      className={styles.paragraph}
-      ref={element}
-      style={{ opacity: scrollYProgress }}
-    >
-      {word.map((word, i) => (
-        <span key={i}>{word}</span>
+    <motion.p className={styles.paragraph} ref={element}>
+      {words.map((word, i) => (
+        <span className={styles.word} key={i}>
+          {word}
+        </span>
       ))}
     </motion.p>
   );
