242431303c0f3aaa226cf21f052d8e400ff9a613
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-02 18:41:12 +0530

    Removed the default font form the layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,5 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
 import HomeButton from "@/components/Home/index";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "My Design Playground",
@@ -13,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <HomeButton />
         {children}
       </body>
