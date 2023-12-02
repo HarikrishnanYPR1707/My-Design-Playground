105dff02c5e5037fbff19f0e1d33c48a02e5f7bc
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-02 17:54:31 +0530

    Changed from absolute to fixed

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -7,21 +7,22 @@ import Link from "next/link";
 import useWindowSize from "@/app/utils/useWindowSize";
 
 const index = () => {
-
   const { innerWidth, innerHeight } = useWindowSize();
 
   return (
     <>
-      {(innerWidth > 1024) ? (
+      {innerWidth > 1024 ? (
         <>
           <Link
             href="/"
-            className="absolute left-10 top-10 z-20 grid aspect-square w-[60px] place-content-center rounded-full bg-[#ec4e39] text-4xl font-black text-[#252525]"
+            className="fixed left-10 top-10 z-20 grid aspect-square w-[60px] place-content-center rounded-full bg-[#ec4e39] text-4xl font-black text-[#252525]"
           >
             <FaHome />
           </Link>
         </>
-      ) : <></>}
+      ) : (
+        <></>
+      )}
     </>
   );
 };
