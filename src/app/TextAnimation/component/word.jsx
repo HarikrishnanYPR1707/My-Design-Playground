66c221234fcbc6c9a69c30b0a16707a4c98448f0
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-02 21:54:02 +0530

    Completed with word animation

--- src/app/TextAnimation/component/word.jsx
+++ src/app/TextAnimation/component/word.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef } from "react";
 import styles from "../page.module.scss";
 
@@ -14,12 +14,26 @@ const word = ({ value }) => {
   const words = value.split(" ");
   return (
     <motion.p className={styles.paragraph} ref={element}>
-      {words.map((word, i) => (
-        <span className={styles.word} key={i}>
-          {word}
-        </span>
-      ))}
+      {words.map((word, i) => {
+        const start = i / words.length;
+        const end = start + 1 / words.length;
+        console.log([start, end]);
+        return (
+          <Word key={i} range={[start, end]} progress={scrollYProgress}>
+            {word}
+          </Word>
+        );
+      })}
     </motion.p>
+  );
+};
+
+const Word = ({ children, range, progress }) => {
+  const opacity = useTransform(progress, range, [0, 1]);
+  return (
+    <motion.span style={{ opacity }} className={styles.word}>
+      {children}
+    </motion.span>
   );
 };
 
