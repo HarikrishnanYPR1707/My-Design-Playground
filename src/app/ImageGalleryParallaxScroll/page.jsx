713adf39f86c841fc2be78e56139e97bbb5f11dd
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-04 17:31:03 +0530

    Modificaiton done

--- src/app/ImageGalleryParallaxScroll/page.jsx
+++ src/app/ImageGalleryParallaxScroll/page.jsx
@@ -1,25 +1,27 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
+import { useTransform, useScroll } from "framer-motion";
+import { useRef } from "react";
+
+const image = [
+  "img1.jpeg",
+  "img2.jpeg",
+  "img3.jpeg",
+  "img4.jpeg",
+  "img5.jpeg",
+  "img6.jpeg",
+  "img7.jpeg",
+  "img8.jpeg",
+  "img9.jpeg",
+  "img10.jpeg",
+  "img11.jpeg",
+  "img12.jpeg",
+];
 
 export default function page() {
-  const image = [
-    "img1.jpeg",
-    "img2.jpeg",
-    "img3.jpeg",
-    "img4.jpeg",
-    "img5.jpeg",
-    "img6.jpeg",
-    "img7.jpeg",
-    "img8.jpeg",
-    "img9.jpeg",
-    "img10.jpeg",
-    "img11.jpeg",
-    "img12.jpeg",
-  ];
-
   return (
     <div className={styles.main}>
-      <div className={styles.gallery}>
+      <div ref={container} className={styles.gallery}>
         <Column images={[image[0], image[1], image[2]]} />
         <Column images={[image[3], image[4], image[5]]} />
         <Column images={[image[6], image[7], image[8]]} />
