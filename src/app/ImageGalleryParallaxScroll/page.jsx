1d94eebbedde66345cf4be18f427d731ec5cc1c8
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-04 17:38:47 +0530

    Added space and text

--- src/app/ImageGalleryParallaxScroll/page.jsx
+++ src/app/ImageGalleryParallaxScroll/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.scss";
 import { useTransform, useScroll } from "framer-motion";
@@ -19,13 +21,26 @@ const image = [
 ];
 
 export default function page() {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end start"],
+  });
+
   return (
     <div className={styles.main}>
+      <div className={styles.spacer}>
+        <p className={styles.para}>Scroll Down</p>
+      </div>
       <div ref={container} className={styles.gallery}>
         <Column images={[image[0], image[1], image[2]]} />
         <Column images={[image[3], image[4], image[5]]} />
         <Column images={[image[6], image[7], image[8]]} />
         <Column images={[image[9], image[10], image[11]]} />
+      </div>
+      <div className={styles.spacer}>
+        <p className={styles.para}>Scroll Up</p>
       </div>
     </div>
   );
