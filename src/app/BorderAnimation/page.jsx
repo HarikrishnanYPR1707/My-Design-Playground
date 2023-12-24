82f9eecaf62822ba8ead2dd12cbc45ea2ae2b6ab
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-25 01:01:06 +0530

    Added inner wrapper styles

--- src/app/BorderAnimation/page.jsx
+++ src/app/BorderAnimation/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex h-screen items-center justify-center border-2">
-      Border Animation
+      <div className="h-[350px] w-[250px] rounded-xl border-2 border-dashed border-cyan-300"></div>
     </div>
   );
 };
