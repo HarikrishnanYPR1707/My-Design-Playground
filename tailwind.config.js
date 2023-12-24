21b10cdc76b1239375df067ea9ea389a43b7ee99
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-25 01:11:20 +0530

    Extended the config for slow animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
       fontFamily: {
         "space-mono": ["Space Mono", "monospace"],
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
   plugins: [],
