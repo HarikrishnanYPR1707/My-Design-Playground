433dcc72af6dd8be77779220fbc6911391dc916f
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-25 01:40:40 +0530

    removed border and add some style for responsiveness

--- src/app/BorderAnimation/page.jsx
+++ src/app/BorderAnimation/page.jsx
@@ -1,13 +1,13 @@
 const page = () => {
   return (
     <div className="flex h-screen items-center justify-center">
-      <div className="relative flex h-[350px] w-[350px] items-center justify-center overflow-hidden rounded-full p-1">
-        <div className="h-full w-full overflow-hidden rounded-full border bg-[#252525] p-4">
+      <div className="relative flex h-[350px] w-[350px] items-center justify-center overflow-hidden rounded-full p-1 duration-300 md:scale-150">
+        <div className="h-full w-full overflow-hidden rounded-full bg-[#252525] p-2">
           <div className="h-full w-full overflow-hidden rounded-full border bg-[#252525]">
             <img src="./myImage.jpg" alt="myImage" className="" />
           </div>
         </div>
-        <div className="animate-spin-slow absolute -z-10 h-[500px] w-[250px]">
+        <div className="animate-spin-slow absolute -z-10 h-[500px] w-[300px]">
           <div className="h-1/2 bg-gradient-to-l from-blue-600 via-purple-500 to-transparent"></div>
           <div className="h-1/2 bg-gradient-to-r from-blue-600 via-purple-500 to-transparent"></div>
         </div>
