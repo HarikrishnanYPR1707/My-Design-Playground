4f6264b95a34f2b02fd4db0a24de52a8d77fdaa7
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-25 01:02:28 +0530

    Added width and height of 100% to our inner top layer div

--- src/app/BorderAnimation/page.jsx
+++ src/app/BorderAnimation/page.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex h-screen items-center justify-center border-2">
-      <div className="h-[350px] w-[250px] rounded-xl border-2 border-dashed border-cyan-300"></div>
+      <div className="h-[350px] w-[250px] rounded-xl border-2 border-dashed border-cyan-300">
+        <div className="h-full w-full"></div>
+      </div>
     </div>
   );
 };
