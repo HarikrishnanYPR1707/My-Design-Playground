35d6ec601a5bee20393e440bd0d1ca0b86bbed3f
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-25 01:04:42 +0530

    Added styles to inner top layer div

--- src/app/BorderAnimation/page.jsx
+++ src/app/BorderAnimation/page.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex h-screen items-center justify-center border-2">
-      <div className="h-[350px] w-[250px] rounded-xl border-2 border-dashed border-cyan-300">
-        <div className="h-full w-full"></div>
+      <div className="flex h-[350px] w-[250px] items-center justify-center rounded-xl border-[1px] border-dashed border-cyan-300 p-1">
+        <div className="h-full w-full rounded-xl border-[1px] border-dashed border-red-300 bg-[#252525]"></div>
       </div>
     </div>
   );
