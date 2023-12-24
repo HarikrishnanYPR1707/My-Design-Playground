42a4bb36ecb603e5a41b3114027053ff7496f682
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-25 00:58:39 +0530

    Added basic styling

--- src/app/BorderAnimation/page.jsx
+++ src/app/BorderAnimation/page.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div className="">Border Animation</div>;
+  return (
+    <div className="flex h-screen items-center justify-center border-2">
+      Border Animation
+    </div>
+  );
 };
 
 export default page;
