3c2acab0882ca280127b2501e4481084e98b7907
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-25 01:37:37 +0530

    completed with border animation

--- src/app/BorderAnimation/page.jsx
+++ src/app/BorderAnimation/page.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-
 const page = () => {
   return (
-    <div className="flex h-screen items-center justify-center border-2">
-      <div className="relative flex h-[350px] w-[250px] items-center justify-center overflow-hidden rounded-xl p-1">
-        <div className="h-full w-full rounded-xl bg-[#252525]"></div>
-        <div className="animate-spin-slow absolute -z-10 h-[500px] w-[150px]">
-          <div className="h-1/2 bg-gradient-to-l from-blue-500 to-transparent"></div>
-          <div className="h-1/2 bg-gradient-to-r from-red-500 to-transparent"></div>
+    <div className="flex h-screen items-center justify-center">
+      <div className="relative flex h-[350px] w-[350px] items-center justify-center overflow-hidden rounded-full p-1">
+        <div className="h-full w-full overflow-hidden rounded-full border bg-[#252525] p-4">
+          <div className="h-full w-full overflow-hidden rounded-full border bg-[#252525]">
+            <img src="./myImage.jpg" alt="myImage" className="" />
+          </div>
+        </div>
+        <div className="animate-spin-slow absolute -z-10 h-[500px] w-[250px]">
+          <div className="h-1/2 bg-gradient-to-l from-blue-600 via-purple-500 to-transparent"></div>
+          <div className="h-1/2 bg-gradient-to-r from-blue-600 via-purple-500 to-transparent"></div>
         </div>
       </div>
     </div>
