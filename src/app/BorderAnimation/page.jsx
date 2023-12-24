f88e51af5e70ae052d1597f70432ed16b81ef476
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2023-12-25 01:15:30 +0530

    Added slow spin animation

--- src/app/BorderAnimation/page.jsx
+++ src/app/BorderAnimation/page.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex h-screen items-center justify-center border-2">
-      <div className="relative flex h-[350px] w-[250px] items-center justify-center overflow-hidden rounded-xl border-[1px] border-dashed border-cyan-300 p-1">
-        <div className="h-full w-full rounded-xl border-[1px] border-dashed border-red-300 bg-[#252525]"></div>
-        <div className="absolute -z-10 h-[500px] w-[150px]">
+      <div className="relative flex h-[350px] w-[250px] items-center justify-center overflow-hidden rounded-xl p-1">
+        <div className="h-full w-full rounded-xl bg-[#252525]"></div>
+        <div className="animate-spin-slow absolute -z-10 h-[500px] w-[150px]">
           <div className="h-1/2 bg-gradient-to-l from-blue-500 to-transparent"></div>
           <div className="h-1/2 bg-gradient-to-r from-red-500 to-transparent"></div>
         </div>
